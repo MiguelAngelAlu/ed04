5a6af929d9a81e99452940ca18625be266399190
Miguel Angel <1873773@alu.murciaeduca.es>
2025-03-14 20:43:19 +0100

    Cambios con el Javadoc en el código

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Lista de contactos de agenda
+ * @author Miguel Ángel
+ * @version 1.0 14/03/2025
+ */
 public class Agenda {
     private List<Contacto> contacts; // Lista de Contacto
 
+    /**
+     *  Muestra un array de números
+     * @return
+     */
     public Agenda() {
         this.contacts = new ArrayList<>();
     }
 
+    /**
+     * Comprueba si el contacto nuevo existe dentro de la agenda, si no está, lo añade.
+     * @param name String con el nombre del contacto
+     * @param phone String con el número del contacto
+     */
     public void addContact(String name, String phone) {
         boolean exists = false;
         for (Contacto c : contacts) {
@@ -27,6 +41,10 @@ public class Agenda {
         }
     }
 
+    /**
+     * Nombre del contacto a eliminar, busca el nombre
+     * @param name String con el nombre a eliminar
+     */
     public void removeContact(String name) {
         Iterator<Contacto> it = contacts.iterator();
 
@@ -39,6 +57,12 @@ public class Agenda {
         }
     }
 
+    /**
+     * Modifica el contacto de la agenda
+     * @param name String con el nombre del contacto
+     * @param oldPhone String con el número antigüo del contacto
+     * @param newPhone String con el número nuev del contacto
+     */
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
         for (Contacto c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
@@ -53,6 +77,10 @@ public class Agenda {
         }
     }
 
+    /**
+     * Muestra los contactos
+     * @return devuelve una lista
+     */
     public List<Contacto> getContacts() {
         return this.contacts;
     }
--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -3,20 +3,39 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Constructor de contacto
+ * @author Miguel Ángel
+ * @version 1.0 14/03/2025
+ */
 class Contacto {
     private String name;
     private List<String> phones;
 
+    /**
+     * Constructor del nombre y el número de teléfono
+     * @param name String con el nombre del contacto
+     * @param phone String con el número del contacto
+     * @return devuelve un array
+     */
     public Contacto(String name, String phone) {
         this.name = name;
         this.phones = new ArrayList<>();
         this.phones.add(phone);
     }
 
+    /**
+     * Getter del nombre
+     * @return devuelve el nombre
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Getter del número
+     * @return Devuelve el número de teléfono
+     */
     public List<String> getPhones() {
         return this.phones;
     }
